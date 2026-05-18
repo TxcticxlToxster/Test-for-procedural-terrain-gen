4771d33c55cb85459d60c467aa8361fb55a9b30a
TxcticxlToxster <jbzigo3@gmail.com>
2026-05-18 09:46:18 +0000

    yay

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import pygame
 import sys
- 
+
+
+
+class Block:
+    def __init__(self,x,y,w,l,rgb):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.length = l
+        self.rgb = rgb
+    
+    def display(self):
+        return (self.x,self.y,self.width,self.length,self.rgb)
+        
+
+def block_display(block):
+    x0 = block.x + camera_x*-1
+    y0 = block.y + camera_y*-1
+    x1 = x0 - block.width
+    y1 = y0 - block.length
+    
+    if (x0 >= 0 and x1 <= WINDOW_WIDTH) and (y0 >= 0 and y1 <= WINDOW_HEIGHT):
+        pygame.draw.rect(screen, block.rgb, pygame.Rect(x0, y0, block.width, block.length))
+        
 # --- Configuration ---
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 FPS = 60
- 
-BG_COLOR = (30, 30, 46)
- 
-# Liste de carrés : (x, y, largeur, hauteur, couleur)
-squares = [
-    (100, 100, 80, 80, (80, 160, 255)),
-    (250, 200, 60, 60, (255, 160, 80)),
-    (400, 150, 100, 100, (100, 220, 120)),
-    (580, 300, 70, 70, (220, 100, 180)),
-]
+BG_COLOR = (255,255,255)
+camera_x = 100
+camera_y = 0
+
+
+
+
+
+
+
+
+
+
+
+display = [Block(0,0,10,10,(0,0,0))]
  
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -29,8 +57,8 @@ while True:
  
     screen.fill(BG_COLOR)
  
-    for (x, y, w, h, color) in squares:
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, w, h))
+    for el in display:
+        block_display(el)
  
     pygame.display.flip()
     clock.tick(FPS)
